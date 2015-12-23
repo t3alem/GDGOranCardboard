08a7dcabf8e81a2d0d8ba1757e1eca8d24c1eaa1
Bouthouri Oussama <oussama.bouthouri@gmail.com>
2015-12-23 09:38:42 +0100

    First commit

---
+++ Assets/Scripts/AutoDestrcution.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestrcution : MonoBehaviour {
+
+	public float dure = 2.0f;
+	private float beginigTime;
+
+	// Use this for initialization
+	void Start () {
+		beginigTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > beginigTime + dure) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LanceRocket.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanceRocket : MonoBehaviour {
+
+	public GameObject rocket;
+	public Transform rocketPosition;
+	public Transform head;
+	// Use this for initialization
+
+	void OnEnable(){
+		Cardboard.SDK.OnTrigger += magnetTrigger;
+	}
+
+	void magnetTrigger(){
+
+		GameObject superRocket  = (GameObject) Instantiate(rocket, rocketPosition.position, rocketPosition.rotation);
+		superRocket.GetComponent<Rigidbody>().AddForce(head.transform.forward * 1000);
+
+	}
+
+	// you can use this also if the first one doesnt work
+	/*void TriggerPulled(){
+
+		GameObject superRocket  = (GameObject) Instantiate(rocket, rocketPosition.position, rocketPosition.rotation);
+		superRocket.GetComponent<Rigidbody>().AddForce(head.transform.forward * 1000);
+	}*/
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+
+			GameObject superRocket  = (GameObject) Instantiate(rocket, rocketPosition.position, rocketPosition.rotation);
+			superRocket.GetComponent<Rigidbody>().AddForce(head.transform.forward * 1000);
+
+		}
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
